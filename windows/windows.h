98713409e695aa5a299b4377eabd0293fb9c019f
Maxim Kupriianov <max@kc.vc>
2016-11-05 23:22:08 +0300

    Windows header dummy.

---
+++ windows/windows.h
@@ -0,0 +1,10 @@
+#ifndef WINDOWS_H
+#define WINDOWS_H
+
+typedef struct HINSTANCE HINSTANCE;
+typedef struct HWND HWND;
+typedef struct HANDLE HANDLE;
+typedef struct SECURITY_ATTRIBUTES SECURITY_ATTRIBUTES;
+typedef struct DWORD DWORD;
+
+#endif // WINDOWS_H
