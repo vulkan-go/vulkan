514f85cf50f95f6056e29540cf7842e073630fd8
neurlang <77860779+neurlang@users.noreply.github.com>
2022-09-09 19:37:46 +0100

    Wayland: Remove vulkan DOT prefix after copy paste
    After copying to the real vulkan package, the vulkan prefix is no longer needed.

--- vulkan_linux.go
+++ vulkan_linux.go
@@ -53,17 +53,17 @@ void wlcallVkGetPhysicalDeviceFeatures2(
 import "C"
 
 // cannot be created without unsafe and C
-var NilPipeline = (*vulkan.Pipeline)(unsafe.Pointer(&C.nilPipeline))
+var NilPipeline = (*Pipeline)(unsafe.Pointer(&C.nilPipeline))
 
 // cannot be created without unsafe and C
-var NilPipelineCache = (*vulkan.PipelineCache)(unsafe.Pointer(&C.nilPipelineCache))
+var NilPipelineCache = (*PipelineCache)(unsafe.Pointer(&C.nilPipelineCache))
 
 // Linux Wayland type flags
 type WaylandSurfaceCreateFlags uint32
 
 // Linux Wayland type struct
 type WaylandSurfaceCreateInfo struct {
-	SType   vulkan.StructureType
+	SType   StructureType
 	PNext   unsafe.Pointer
 	Flags   WaylandSurfaceCreateFlags
 	Display uintptr
@@ -71,24 +71,24 @@ type WaylandSurfaceCreateInfo struct {
 }
 
 // CreateInstance function as declared in https://www.khronos.org/registry/vulkan/specs/1.0/man/html/vkCreateInstance.html
-func CreateInstance(pCreateInfo *vulkan.InstanceCreateInfo, pAllocator *vulkan.AllocationCallbacks, pInstance *vulkan.Instance) vulkan.Result {
+func CreateInstance(pCreateInfo *InstanceCreateInfo, pAllocator *AllocationCallbacks, pInstance *Instance) Result {
 	cpCreateInfo, _ := pCreateInfo.PassRef()
 	cpAllocator, _ := (*C.VkAllocationCallbacks)(unsafe.Pointer(pAllocator)), 0
 	cpInstance, _ := (*C.VkInstance)(unsafe.Pointer(pInstance)), 0
 	__ret := C.wlcallVkCreateInstance(unsafe.Pointer(cpCreateInfo), cpAllocator, cpInstance)
-	__v := (vulkan.Result)(__ret)
+	__v := (Result)(__ret)
 	return __v
 }
 
 // GetPhysicalDeviceFeatures2 function as declared in https://www.khronos.org/registry/vulkan/specs/1.0/man/html/vkGetPhysicalDeviceFeatures2.html
-func GetPhysicalDeviceFeatures2(physicalDevice vulkan.PhysicalDevice, pFeatures *vulkan.PhysicalDeviceFeatures2) {
+func GetPhysicalDeviceFeatures2(physicalDevice PhysicalDevice, pFeatures *PhysicalDeviceFeatures2) {
 	cphysicalDevice, _ := *(*C.VkPhysicalDevice)(unsafe.Pointer(&physicalDevice)), 0
 	cpFeatures, _ := pFeatures.PassRef()
 	C.wlcallVkGetPhysicalDeviceFeatures2(cphysicalDevice, unsafe.Pointer(cpFeatures))
 }
 
 // CreateWaylandSurface is a Linux Wayland-related function
-func CreateWaylandSurface(instance vulkan.Instance, info *WaylandSurfaceCreateInfo, pAllocator *vulkan.AllocationCallbacks, pSurface *vulkan.Surface) {
+func CreateWaylandSurface(instance Instance, info *WaylandSurfaceCreateInfo, pAllocator *AllocationCallbacks, pSurface *Surface) {
 	cpAllocator, _ := (*C.VkAllocationCallbacks)(unsafe.Pointer(pAllocator)), 0
 	cpSurface, _ := (*C.VkSurfaceKHR)(unsafe.Pointer(pSurface)), 0
 
@@ -96,6 +96,6 @@ func CreateWaylandSurface(instance vulkan.Instance, info *WaylandSurfaceCreateIn
 }
 
 // GetPhysicalDeviceWaylandPresentationSupport is a Linux Wayland-related function
-func GetPhysicalDeviceWaylandPresentationSupport(physicalDevice vulkan.PhysicalDevice, queueFamilyIndex uint32, display uintptr) bool {
+func GetPhysicalDeviceWaylandPresentationSupport(physicalDevice PhysicalDevice, queueFamilyIndex uint32, display uintptr) bool {
 	return 0 != C.wlcallVkGetPhysicalDeviceWaylandPresentationSupportKHR(unsafe.Pointer(physicalDevice), C.uint(queueFamilyIndex), (*C.struct_wl_display)(unsafe.Pointer(display)))
 }
